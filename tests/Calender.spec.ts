9df62dc9f2592a3fdfb5f0f45a4d37122fe2e252
Aamir Mithabhai <aamir.mithabhai@allata.com>
2026-01-20 17:25:52 +0530

    Calandar Handelling

--- tests/Calender.spec.ts
+++ tests/Calender.spec.ts
@@ -4,10 +4,32 @@ test("Calneder handeling", async ({page})=>{
 
     await page.goto("https://www.hyrtutorials.com/p/calendar-practice.html")
 
-    let targetDate ='10'
-    let targetMonth = 'May'
+    await page.locator('.ui-datepicker-trigger').click()
+
+
+    let targetDate ="10"
+    let targetMonth = "May"
     let targetYear = "2027"
 
-    
+    const monthPicker = page.locator('.ui-datepicker-month')
+    const yearPicker = page.locator('.ui-datepicker-year')
+
+    while(true){
+
+        if((await monthPicker.textContent() === targetMonth) && (await yearPicker.textContent() === targetYear))
+
+         {
+               await page.getByText(targetDate, {exact:true}).click()
+               break
+         } 
+         
+         else{
+
+              await page.getByText('Next', {exact:true}).click()
+         }
+    }
+
+
+    await page.waitForTimeout(3000)
 
 })
